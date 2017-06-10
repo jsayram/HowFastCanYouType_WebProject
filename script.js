ef9b663a33dd2e3934e598bd586865b41a7d8499
Jose Ramirez <jsayram@Gmail.com>
2017-06-09 20:44:16 -0400

    Got timer to count up properly for the minutes, seconds, and thousandths of a second, now just need to format

--- script.js
+++ script.js
@@ -5,11 +5,22 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
 
+var timer = [0, 0, 0, 0]; // this array will capture the minutes, seconds, hundethds, & thousandths.
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
+function runTimer() {
+    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2]; //this is the clock setu m,s,hund
+    theTimer.innerHTML = currentTime;
+    timer[3]++; //this updates the 4th postion on the array
 
+    //Math.floor means you just wont get any decimals
+    timer[0] = Math.floor((timer[3] / 100) / 60); //this will give just seconds/60 to give minutes
+    timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60)) //everytime we hit 60 sec this value goes to 0
+    /*thousanths of second - the seconds*100 - minutes*6000, that way we clear out every time we get to a hundredth of a second, and also subract the minutes*6000 , thats every time the minutes reach 100 so we dont count higher than there.*/
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
@@ -20,6 +31,9 @@ function spellCheck() {
 // Start the timer:
 function start() {
     let textEnteredLength = testArea.value.length;
+    if (textEnteredLength === 0) {
+        setInterval(runTimer, 10); //this will run the every 1000 of a second
+    }
     console.log(textEnteredLength);
 }
 
